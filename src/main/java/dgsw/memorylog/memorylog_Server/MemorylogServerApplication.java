f929dc11fa2874c2a3793f9ab74de45daa4e2003
jjmin321 <jjmin321@naver.com>
2020-09-14 15:45:44 +0900

    [UPDATE] All

---
+++ src/main/java/dgsw/memorylog/memorylog_Server/MemorylogServerApplication.java
@@ -0,0 +1,13 @@
+package dgsw.memorylog.memorylog_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorylogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorylogServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dgsw/memorylog/memorylog_Server/MemorylogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dgsw.memorylog.memorylog_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorylogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
