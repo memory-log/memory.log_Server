dbf87ea3a42b86049875c350480c88abd21f7f87
jjmin321 <jjmin321@naver.com>
2020-10-15 11:16:30 +0900

    [IMPROVE] Database/Vo

--- src/main/java/dgsw/memorylog/memorylog_Server/controller/MemberController.java
+++ src/main/java/dgsw/memorylog/memorylog_Server/controller/MemberController.java
@@ -2,6 +2,7 @@ package dgsw.memorylog.memorylog_Server.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dgsw.memorylog.memorylog_Server.database.vo.member.MemberLoginVo;
+import dgsw.memorylog.memorylog_Server.lib.MakeJson;
 import dgsw.memorylog.memorylog_Server.service.Member.MemberServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,19 +14,25 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/member")
 public class MemberController {
+
     @Autowired
     private MemberServiceImpl memberService;
-    @GetMapping("/login")
+
+    MakeJson makeJson = new MakeJson();
+
+    @GetMapping("/signin")
     @ResponseBody()
     public Object login(@RequestBody @Valid MemberLoginVo memberLoginVo) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            HashMap<String, String> map = new HashMap<String, String>();
-            map.put("사용자의 이메일", memberLoginVo.getEmail());
-            map.put("사용자의 패스워드", memberLoginVo.getPw());
-            return mapper.writeValueAsString(map);
+            Integer memberIdx = memberService.login(memberLoginVo);
+            makeJson.setJson("Status", 200);
+            makeJson.setJson("Idx", memberIdx);
+            return makeJson.getJson();
         }catch (Exception e){
-            return 0;
+            makeJson.setJson("Status", 400);
+            makeJson.setJson("Idx", "존재하지 않는 회원");
+            return makeJson.getJson();
         }
     }
+
 }
--- src/main/java/dgsw/memorylog/memorylog_Server/database/vo/member/MemberSignUpVo.java
+++ src/main/java/dgsw/memorylog/memorylog_Server/database/vo/member/MemberSignUpVo.java
@@ -1,17 +1,18 @@
 package dgsw.memorylog.memorylog_Server.database.vo.member;
 
-import com.sun.istack.NotNull;
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
-/**
- * 멤버 로그인 오브젝트
- */
 @Getter
-public class MemberLoginVo {
+public class MemberSignUpVo {
+    @NotBlank
+    private Integer idx;
+
+    @NotBlank
+    private String name;
+
     @Email
     @NotBlank
     private String email;
