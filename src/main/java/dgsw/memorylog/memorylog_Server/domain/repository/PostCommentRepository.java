a67382ce253a394fce9662f7e6e3a3b1fd9af900
Sh031224 <1cktmdgh2@gmail.com>
2020-10-28 21:16:07 +0900

    Fix: Merge

--- src/main/java/dgsw/memorylog/memorylog_Server/domain/repository/PostCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package dgsw.memorylog.memorylog_Server.domain.repository;
-
-import dgsw.memorylog.memorylog_Server.domain.entity.PostComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostCommentRepository extends JpaRepository<PostComment, Integer> {
-}
